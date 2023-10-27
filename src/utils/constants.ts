874d43f13ee85e1720d682b6f67e4a93194deacc
ehab-gamal-abdelwahab <ehab.gamal74@gmail.com>
2023-10-27 14:57:56 +0200

    test

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,9 @@
-export const DOMAIN = 'http://ec2-44-204-28-7.compute-1.amazonaws.com:3000';
+export const DOMAIN = "http://ec2-44-204-28-7.compute-1.amazonaws.com:3000";
 
 export const apiTokens = {
-  login: '/login',
-  register: '/register',
-  users: '/user',
-  locations: '/location',
+  login: "/login",
+  register: "/register",
+  users: "/user",
+  locations: "/location",
+  test: "/location",
 };
